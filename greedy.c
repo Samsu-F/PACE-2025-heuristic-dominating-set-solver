11dbeeda484288adb0a6a1b8a966b54c9aa965ae
Samsu-F <samsu-f@web.de>
2025-06-03 21:25:54 +0200

    refactor greedy

--- greedy.c
+++ greedy.c
@@ -3,17 +3,18 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <signal.h>
 
 #include "assert_allow_float_equal.h"
 
 
 
-size_t g_reconstruct_counter = 0;
+static bool _g_sigterm_received = false;
 
 
 
 // return the new ds size
-size_t _make_minimal(Graph* g, size_t current_ds_size)
+static size_t _make_minimal(Graph* g, size_t current_ds_size)
 {
     assert(g != NULL);
     for(size_t i_vertices = 0; i_vertices < g->n; i_vertices++) {
@@ -43,76 +44,9 @@ size_t _make_minimal(Graph* g, size_t current_ds_size)
 
 
 // comparison function for the priority queue
-bool double_greater(const double a, const double b)
+static bool _double_greater(const double a, const double b)
 {
     return a > b;
-}
-
-
-
-size_t greedy(Graph* g)
-{
-    uint32_t undominated_vertices = 0; // the total number of undominated vertices remaining in the graph
-    PQueue* pq = pq_new(double_greater);
-    if(!pq) {
-        perror("greedy: pq_new failed");
-        exit(EXIT_FAILURE);
-    }
-    for(uint32_t vertices_idx = 0; vertices_idx < g->n; vertices_idx++) {
-        Vertex* v = g->vertices[vertices_idx];
-        assert(!v->is_in_pq);               // this is the same data as v->is_removed
-        uint32_t undominated_neighbors = 0; // including itself if v is undominated
-        if(v->dominated_by_number == 0) {
-            undominated_vertices++;
-            undominated_neighbors++;
-        }
-        for(uint32_t i = 0; i < v->degree; i++) {
-            if(v->neighbors[i]->dominated_by_number == 0) {
-                undominated_neighbors++;
-            }
-        }
-        pq_insert(pq, (KeyValPair) {.key = undominated_neighbors, .val = v});
-    }
-
-    size_t current_ds_size = 0;
-    while(undominated_vertices > 0) {
-        assert(!pq_is_empty(pq));
-        KeyValPair kv = pq_pop(pq);
-        Vertex* v = kv.val;
-        assert(!v->is_in_ds);
-        v->is_in_ds = true;
-        current_ds_size++;
-        uint32_t v_is_newly_dominated = 0;
-        v->dominated_by_number++;
-        if(v->dominated_by_number == 1) {
-            v_is_newly_dominated = 1;
-            undominated_vertices--;
-        }
-
-        for(uint32_t i_v = 0; i_v < v->degree; i_v++) {
-            Vertex* u1 = v->neighbors[i_v];
-            u1->dominated_by_number++;
-            uint32_t delta_undominated_neighbors_u1 = v_is_newly_dominated;
-            if(u1->dominated_by_number == 1) {    // if v is the first one to dominate u1
-                delta_undominated_neighbors_u1++; // u1 lost itself as an undominated neighbor
-                undominated_vertices--;
-                for(uint32_t i_u1 = 0; i_u1 < u1->degree; i_u1++) {
-                    Vertex* u2 = u1->neighbors[i_u1];
-                    // because u1 is now dominated, u2 has one undominated neighbor fewer than before
-                    if(u2->is_in_pq) {
-                        pq_decrease_priority(pq, u2, pq_get_key(pq, u2) - 1);
-                    }
-                }
-            }
-            // u1 lost 0, 1, or 2 undominated neighbors, depending on if u1 and v were undominated before
-            if(u1->is_in_pq && delta_undominated_neighbors_u1 > 0) {
-                pq_decrease_priority(pq, u1, pq_get_key(pq, u1) - delta_undominated_neighbors_u1);
-            }
-        }
-    }
-    pq_free(pq);
-    current_ds_size = _make_minimal(g, current_ds_size);
-    return current_ds_size;
 }
 
 
@@ -146,6 +80,7 @@ static size_t _remove_randomly_from_ds(Graph* g, double removal_probability, siz
 
 
 
+// TODO: use more efficient queue implementation specifically designed for the use case
 typedef struct Queue {
     struct Queue* next;
     Vertex* val;
@@ -153,6 +88,7 @@ typedef struct Queue {
 
 
 
+// TODO: refactor
 // returns the resulting ds size
 static size_t _local_deconstruction(Graph* g, const size_t current_ds_size)
 {
@@ -213,89 +149,6 @@ static size_t _local_deconstruction(Graph* g, const size_t current_ds_size)
 
 
 
-// ugly, just temporary
-// returns the resulting ds size
-size_t greedy_remove_and_refill(Graph* g, double removal_probability, size_t current_ds_size)
-{
-    assert(g != NULL);
-    assert(removal_probability > 0.0 && removal_probability < 1.0);
-    uint32_t undominated_vertices = 0; // the total number of undominated vertices remaining in the graph
-
-    if(++g_reconstruct_counter % 3 == 0) {
-        assert(fprintf(stderr, "local deconstruction \t"));
-        current_ds_size = _local_deconstruction(g, current_ds_size);
-    }
-    else {
-        assert(fprintf(stderr, "random deconstruction\t"));
-        current_ds_size = _remove_randomly_from_ds(g, removal_probability, current_ds_size);
-    }
-
-    PQueue* pq = pq_new(double_greater);
-    if(!pq) {
-        perror("greedy: pq_new failed");
-        exit(EXIT_FAILURE);
-    }
-    for(uint32_t vertices_idx = 0; vertices_idx < g->n; vertices_idx++) {
-        Vertex* v = g->vertices[vertices_idx];
-        uint32_t undominated_neighbors = 0; // including itself if v is undominated
-        if(v->dominated_by_number == 0) {
-            undominated_vertices++;
-            undominated_neighbors++;
-        }
-        for(uint32_t i = 0; i < v->degree; i++) {
-            if(v->neighbors[i]->dominated_by_number == 0) {
-                undominated_neighbors++;
-            }
-        }
-        v->is_in_pq = false;
-        if(undominated_neighbors > 0) {
-            pq_insert(pq, (KeyValPair) {.key = undominated_neighbors, .val = v});
-        }
-    }
-
-
-    while(undominated_vertices > 0) {
-        assert(!pq_is_empty(pq));
-        KeyValPair kv = pq_pop(pq);
-        Vertex* v = kv.val;
-        assert(!v->is_in_ds);
-        v->is_in_ds = true;
-        current_ds_size++;
-        uint32_t v_is_newly_dominated = 0;
-        v->dominated_by_number++;
-        if(v->dominated_by_number == 1) {
-            v_is_newly_dominated = 1;
-            undominated_vertices--;
-        }
-
-        for(uint32_t i_v = 0; i_v < v->degree; i_v++) {
-            Vertex* u1 = v->neighbors[i_v];
-            u1->dominated_by_number++;
-            uint32_t delta_undominated_neighbors_u1 = v_is_newly_dominated;
-            if(u1->dominated_by_number == 1) {    // if v is the first one to dominate u1
-                delta_undominated_neighbors_u1++; // u1 lost itself as an undominated neighbor
-                undominated_vertices--;
-                for(uint32_t i_u1 = 0; i_u1 < u1->degree; i_u1++) {
-                    Vertex* u2 = u1->neighbors[i_u1];
-                    // because u1 is now dominated, u2 has one undominated neighbor fewer than before
-                    if(u2->is_in_pq) {
-                        pq_decrease_priority(pq, u2, pq_get_key(pq, u2) - 1);
-                    }
-                }
-            }
-            // u1 lost 0, 1, or 2 undominated neighbors, depending on if u1 and v were undominated before
-            if(u1->is_in_pq && delta_undominated_neighbors_u1 > 0) {
-                pq_decrease_priority(pq, u1, pq_get_key(pq, u1) - delta_undominated_neighbors_u1);
-            }
-        }
-    }
-    pq_free(pq);
-    current_ds_size = _make_minimal(g, current_ds_size);
-    return current_ds_size;
-}
-
-
-
 void init_votes(Graph* g)
 {
     assert(g != NULL);
@@ -307,25 +160,11 @@ void init_votes(Graph* g)
 
 
 
-size_t greedy_vote_remove_and_refill(Graph* g, double removal_probability, size_t current_ds_size)
+static size_t _greedy_vote_construct(Graph* g, size_t current_ds_size)
 {
-    assert(g != NULL);
-    assert(removal_probability > 0.0 && removal_probability < 1.0);
-
-    init_votes(g); // TODO: this is only necessary once, not every IG iteration step
-
     uint32_t undominated_vertices = 0; // the total number of undominated vertices remaining in the graph
 
-    if(++g_reconstruct_counter % 3 == 0) {
-        assert(fprintf(stderr, "local deconstruction \t"));
-        current_ds_size = _local_deconstruction(g, current_ds_size);
-    }
-    else {
-        assert(fprintf(stderr, "random deconstruction\t"));
-        current_ds_size = _remove_randomly_from_ds(g, removal_probability, current_ds_size);
-    }
-
-    PQueue* pq = pq_new(double_greater);
+    PQueue* pq = pq_new(_double_greater);
     if(!pq) {
         perror("greedy: pq_new failed");
         exit(EXIT_FAILURE);
@@ -368,8 +207,8 @@ size_t greedy_vote_remove_and_refill(Graph* g, double removal_probability, size_
             Vertex* u1 = v->neighbors[i_v];
             u1->dominated_by_number++;
             double delta_weight_u1 = v_is_newly_dominated * v->vote;
-            if(u1->dominated_by_number == 1) {    // if v is the first one to dominate u1
-                delta_weight_u1 += u1->vote; // u1 no longer votes for itself
+            if(u1->dominated_by_number == 1) { // if v is the first one to dominate u1
+                delta_weight_u1 += u1->vote;   // u1 no longer votes for itself
                 undominated_vertices--;
                 for(uint32_t i_u1 = 0; i_u1 < u1->degree; i_u1++) {
                     Vertex* u2 = u1->neighbors[i_u1];
@@ -387,4 +226,118 @@ size_t greedy_vote_remove_and_refill(Graph* g, double removal_probability, size_
     pq_free(pq);
     current_ds_size = _make_minimal(g, current_ds_size);
     return current_ds_size;
+}
+
+
+
+// just temporary, not good
+static void _sigterm_handler(int sig)
+{
+    (void)sig;
+    _g_sigterm_received = true;
+}
+
+
+
+static void _register_sigterm_handler(void)
+{
+    struct sigaction sa;
+    sa.sa_handler = _sigterm_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART; // 0 or SA_RESTART if syscalls should be restarted
+    if(sigaction(SIGTERM, &sa, NULL) == -1) {
+        perror("sigaction failed");
+        exit(EXIT_FAILURE);
+    }
+}
+
+
+
+static void _print_solution(Graph* g, size_t ds_size)
+{
+    assert(g != NULL && g->vertices != NULL && g->fixed.vertices != NULL);
+    printf("%zu\n", g->fixed.size + ds_size);
+    for(size_t fixed_idx = 0; fixed_idx < g->fixed.size; fixed_idx++) {
+        Vertex* v = g->fixed.vertices[fixed_idx];
+        printf("%" PRIu32 "\n", v->id);
+    }
+#ifndef NDEBUG
+    size_t ds_vertices_found_in_g = 0; // this variable is just for an assertion
+#endif
+    for(size_t i_vertices = 0; i_vertices < g->n; i_vertices++) {
+        Vertex* v = g->vertices[i_vertices];
+        if(v->is_in_ds) {
+            printf("%" PRIu32 "\n", v->id);
+#ifndef NDEBUG
+            ds_vertices_found_in_g++;
+#endif
+        }
+    }
+    fflush(stdout);
+    assert(ds_vertices_found_in_g == ds_size);
+}
+
+
+
+// runs iterated greedy algorithm on the graph until a sigterm signal is received
+void iterated_greedy_solver(Graph* g)
+{
+    const double removal_probability = 0.05; // can be tweaked
+    _register_sigterm_handler();
+    init_votes(g);
+    // create two array that are use to save and restore the best solution found so far:
+    bool* in_ds = calloc(g->n, sizeof(bool));
+    uint32_t* dominated_by_numbers = malloc(g->n * sizeof(uint32_t));
+    if(in_ds == NULL || dominated_by_numbers == NULL) {
+        exit(1);
+    }
+
+    size_t current_ds_size = _greedy_vote_construct(g, 0); // get initial solution
+    for(uint32_t i = 0; i < g->n; i++) {                   // save the initial solution
+        dominated_by_numbers[i] = g->vertices[i]->dominated_by_number;
+        in_ds[i] = g->vertices[i]->is_in_ds;
+    }
+    size_t saved_ds_size = current_ds_size; // the size of the ds saved in dominated_by_numbers and in_ds
+
+    size_t ig_iteration = 0;
+    for(; !_g_sigterm_received; ig_iteration++) {
+        // deconstruct solution
+        if(ig_iteration % 3 == 0) {
+            assert(fprintf(stderr, "local deconstruction \t"));
+            current_ds_size = _local_deconstruction(g, current_ds_size);
+        }
+        else {
+            assert(fprintf(stderr, "random deconstruction\t"));
+            current_ds_size = _remove_randomly_from_ds(g, removal_probability, current_ds_size);
+        }
+        // reconstruct
+        current_ds_size = _greedy_vote_construct(g, current_ds_size);
+
+        if(current_ds_size <= saved_ds_size) {
+            assert(fprintf(stderr, "%s current_ds_size == %zu\tsaved_ds_size == %zu\t\tig_iteration == %zu\n",
+                           current_ds_size < saved_ds_size ? "IMPROVEMENT:" : "EQUAL: =    ", current_ds_size,
+                           saved_ds_size, ig_iteration));
+            for(uint32_t i = 0; i < g->n; i++) { // save the current solution
+                dominated_by_numbers[i] = g->vertices[i]->dominated_by_number;
+                in_ds[i] = g->vertices[i]->is_in_ds;
+            }
+            saved_ds_size = current_ds_size;
+        }
+        else { // restore saved solution
+            assert(fprintf(stderr, "worse:       current_ds_size == %zu\tsaved_ds_size == %zu\t\tig_iteration == %zu\n",
+                           current_ds_size, saved_ds_size, ig_iteration));
+            for(uint32_t i = 0; i < g->n; i++) {
+                g->vertices[i]->dominated_by_number = dominated_by_numbers[i];
+                g->vertices[i]->is_in_ds = in_ds[i];
+            }
+            current_ds_size = saved_ds_size;
+        }
+    }
+    fprintf(stderr, "g_sigterm_received == %d\t\tfinal ds size == %zu   \tgreedy iterations == %zu\n",
+            _g_sigterm_received, current_ds_size, ig_iteration);
+    fflush(stderr);
+    _print_solution(g, current_ds_size);
+
+    free(in_ds);
+    free(dominated_by_numbers);
 }
